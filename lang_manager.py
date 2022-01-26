143df3ed491d4da467276e744087b32ef3a87e41
leifadev <49649192+leifadev@users.noreply.github.com>
2022-01-26 17:54:50 -0600

    expanded on request function, pretty much fully functional!

--- lang_manager.py
+++ lang_manager.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import requests, os, getpass
-import pprint as pprint
-
+import logging
+from pprint import pprint
 
 # For cacheing, fetching, and managing data, and more from the Free Dictionary API ==> https://dictionaryapi.dev/
 
@@ -18,53 +18,59 @@ class data:
 
 class Manager():
 
-    # Requests
+    # You must initialize logging, otherwise you'll not see debug output
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.DEBUG)
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.DEBUG)
+    requests_log.propagate = True
+
+
+    # Requests all relevant data for specified word and language, returns it in >>> self.output! <<<
     def request(self, lang, word):
         api_address = f"https://api.dictionaryapi.dev/api/v2/entries/{lang}/{word}"
         # print(api_address) # print output for debug
 
-        self.coreDefinings = [
-                "word",
-                "phonetic",
-                "origin",
-                "partOfSpeech",
-                "definition",
-                "example"
-            ]
+        self.coreDefinings = {
+                "word": "",
+                "partOfSpeech": "",
+                "phonetic": "",
+                "definition": "",
+                "example": "",
+                "origin": ""
+            }
 
 
         try:
-            self.output = []
+            self.output = {}
 
             # Fetch definition data: word, phonetics, origin, part of speech, definition, example
-
             data = requests.get(api_address).json()
             for i in data[0]:
-                if i in self.coreDefinings:
-                    output = print((data[0][i]))
-                    self.output.append(output)
+                if i in self.coreDefinings.keys():
+                    output = (data[0][i])
+                    if output != None:
+                        # print(i, output)
+                        self.output.update({i:output})
 
-            meanings = data[0]["meanings"][0]
-            self.output.append(meanings["partOfSpeech"])
-            self.output.append(meanings["definitions"][0]["definition"])
-            for l in self.output:
-                if l is None:
-                    self.output.remove(l)
+            meanings = data[0]["meanings"][0] # abreviation for the path to get too the definition stuff
+            self.output.update({"partOfSpeech":meanings["partOfSpeech"]})
+            self.output.update({"definition":meanings["definitions"][0]["definition"]})
+            self.output.update({"example":meanings["definitions"][0]["example"]})
 
-            print(meanings)
+            pprint(f'{self.output}') # print filtered output of my desired objects for shoutout
 
-        except requests.exceptions.ConnectionError as e:
+        except ConnectionError as e:
             print(f"Error!\n{e}")
+        except KeyError as l:
+            print(f'There was a error with finding objects! A API object wasn\'t available (e.g. example)\n{l}')
 
-
+    def write(self, input):
+        pass
 
 
 
 # Invoked Request...
 nice = Manager()
-nice.request("en", "pole")
-
-
-
-langAPI = [{'word': 'pole', 'phonetic': 'pəʊl', 'phonetics': [{'text': 'pəʊl', 'audio': '//ssl.gstatic.com/dictionary/static/sounds/20200429/pole--1_gb_3.mp3'}], 'origin': 'late Old English pāl (in early use without reference to thickness or length), of Germanic origin; related to Dutch paal and German Pfahl, based on Latin palus ‘stake’.', 'meanings': [{'partOfSpeech': 'noun', 'definitions': [{'definition': 'a long, slender, rounded piece of wood or metal, typically used with one end placed in the ground as a support for something.', 'example': 'a tent pole', 'synonyms': ['post', 'pillar', 'stanchion', 'standard', 'paling', 'pale', 'stake', 'stick', 'picket', 'palisade', 'support', 'prop', 'batten', 'mast', 'bar', 'shaft', 'rail', 'rod', 'beam', 'spar', 'crosspiece', 'upright', 'vertical', 'staff', 'stave', 'cane', 'spike', 'baton', 'truncheon'], 'antonyms': []}, {'definition': 'another term for perch3 (sense 1 of the noun).', 'synonyms': [], 'antonyms': []}]}, {'partOfSpeech': 'verb', 'definitions': [{'definition': 'propel (a boat) by pushing a pole against the bottom of a river, canal, or lake.', 'example': 'the boatman appeared, poling a small gondola', 'synonyms': [], 'antonyms': []}]}]}, {'word': 'pole', 'phonetic': 'pəʊl', 'phonetics': [{'text': 'pəʊl', 'audio': '//ssl.gstatic.com/dictionary/static/sounds/20200429/pole--1_gb_3.mp3'}], 'origin': 'late Middle English: from Latin polus ‘end of an axis’, from Greek polos ‘pivot, axis, sky’.', 'meanings': [{'partOfSpeech': 'noun', 'definitions': [{'definition': 'either of the two locations ( North Pole or South Pole ) on the surface of the earth (or of a celestial object) which are the northern and southern ends of the axis of rotation.', 'synonyms': [], 'antonyms': []}]}]}, {'word': 'pole', 'phonetic': 'pəʊl', 'phonetics': [{'text': 'pəʊl', 'audio': '//ssl.gstatic.com/dictionary/static/sounds/20200429/pole--1_gb_3.mp3'}], 'meanings': [{'partOfSpeech': 'noun', 'definitions': [{'definition': 'short for pole position.', 'synonyms': [], 'antonyms': []}]}]}, {'word': 'Pole', 'phonetic': 'pəʊl', 'phonetics': [{'text': 'pəʊl', 'audio': '//ssl.gstatic.com/dictionary/static/sounds/20200429/pole--1_gb_3.mp3'}], 'origin': 'via German from Polish Polanie, literally ‘field-dwellers’, from pole ‘field’.', 'meanings': [{'partOfSpeech': 'noun', 'definitions': [{'definition': 'a native or inhabitant of Poland, or a person of Polish descent.', 'synonyms': [], 'antonyms': []}]}]}]
+nice.request("en", "verbose")
 
