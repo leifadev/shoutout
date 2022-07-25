fb1d6ba26296678e7bbc5a46abbcf3ac4b238954
leifadev <49649192+leifadev@users.noreply.github.com>
2022-07-24 21:23:32 -0500

    took out 3 unneeded modules for now

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 import WindowController
 import AppDelegate
 import notifications
-import tasks
-import lang_manager
-import daemon
+# import tasks
+# import lang_manager
+# import daemon
+
 
 if __name__ == "__main__":
     from PyObjCTools import AppHelper
