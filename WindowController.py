7668330182f2e9fb5fd6dffa945762b5c52702d5
leifadev <49649192+leifadev@users.noreply.github.com>
2022-09-21 11:51:52 -0500

    inherit from preferences window controller, also inherits Cocoa/Foundation classes, removed unneeded funcs

--- WindowController.py
+++ WindowController.py
@@ -1,94 +1,54 @@
+# Main Modules
 import os
 
 # Objective-C
 import Cocoa, objc
 from AppKit import NSApp
 
+# Inherit from preferences subclass of NSWindowController
+from prefController import prefWindow
 
-# Global stuff
-version = "beta~v0.0.1"
 
+class mainWindow(prefWindow):
+    """
+    Main Controller for main window
 
-class mainWindow(Cocoa.NSWindowController):
+    (Inherits from prefWindow class, prefController.py)
+    """
 
-    outlet = objc.IBOutlet()
+    # Outlets
 
     def __init__(self):
-        self.theme = ["dark", "light"]
-        self.count = 0
-        self.definitionFull = {
-            "word": "",
-            "definition": "",
-            "phoentics": "",
-            "pronounceAudio": ""
-        }
-
-    def windowDidLoad(self):
-        Cocoa.NSWindowController.windowDidLoad(self)
-
+        self.cool = "cool"
+        self.version = "beta~v0.0.1"
+        word = objc.IBOutlet()
+        definition = objc.IBOutlet()
+        example = objc.IBOutlet()
 
     @objc.IBAction
     def helplink_(self, sender):
         url = ("https://github.com/leifadev/shoutout/wiki")
         print(f"{url} launched")
-        x = Cocoa.NSURL.alloc().initWithString_(url)
-        Cocoa.NSWorkspace.alloc().openURL_(x)
-
-    # Increment numbers display stuff (from dawes.wordpress.com)
-    @objc.IBAction
-    def increment_(self, sender):
-        self.count += 1
-        print(self.count)
-        self.updateDisplay()
-
-    @objc.IBAction
-    def decrement_(self, sender):
-        self.count -= 1
-        print(self.count)
-        self.updateDisplay()
-
-    def updateDisplay(self):
-        self.outlet.setStringValue_(self.count)
-
-    @objc.IBAction
-    def showPref_(self, sender):
-        rwc = prefWindow.alloc().initWithWindowNibName_(u"shoutout_preferences")
-        rwc.showWindow_(rwc)
+        link = Cocoa.NSURL.alloc().initWithString_(url)
+        Cocoa.NSWorkspace.alloc().openURL_(link)
 
 
-class prefWindow(Cocoa.NSWindowController):
+class UIElements:
+    pass
 
-    outlet = objc.IBOutlet()
 
-    def __init__(self):
-        self.theme = ["dark", "light"]
-        self.complexity = [1, 2, 3]
-        self.notification_config = {
-         "perDay": 1,
-         "delay": 360 # 6 hours, **in minutes**
-        }
-
-    @objc.IBAction
-    def openlink_(self, sender):
-        url = ("https://github.com/leifadev/shoutout/")
-        print(f"{url} launched")
-        x = Cocoa.NSURL.alloc().initWithString_(url)
-        Cocoa.NSWorkspace.alloc().openURL_(x)
 
 
 # Starting window!
 
-# if __name__ == "__main__":
-#
-#     # Initiate the controller with a XIB
-#     viewController = mainWindow.alloc().initWithWindowNibName_("shoutout_main")
-#
-#     # Show the window
-#     viewController.showWindow_(viewController)
-#
-#     # Bring app to top
-#     NSApp.activateIgnoringOtherApps_(True)
-#
-#     from PyObjCTools import AppHelper
-#     AppHelper.runEventLoop()
+if __name__ == "__main__":
+
+    # Initiate the controller with a XIB
+    viewController = mainWindow.alloc().initWithWindowNibName_("shoutout_main")
+
+    # Show the window
+    viewController.showWindow_(viewController)
+
+    # Bring app to top
+    NSApp.activateIgnoringOtherApps_(True)
 
