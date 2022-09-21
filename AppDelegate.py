694c8e3586d28dc2fd868a60a260b5ee6a122409
leifadev <49649192+leifadev@users.noreply.github.com>
2022-09-21 12:01:55 -0500

    correct pyobjc syntax

--- AppDelegate.py
+++ AppDelegate.py
@@ -6,23 +6,23 @@ import Cocoa, objc
 from WindowController import mainWindow
 from AppKit import *
 
-
 class AppDelegate(Cocoa.NSObject):
     """
     App delegate for Shoutout!
     """
-
     def __init__(self):
-        self.app = Cocoa.NSApplication.sharedApplication()
-        # Initiate the controller with a XIB
-        viewController = mainWindow.alloc().initWithWindowNibName_("shoutout_main")
-        viewController.showWindow_(viewController)
+        pass
 
-        # Bring app to top
-        NSApp.activateIgnoringOtherApps_(True)
+    app = Cocoa.NSApplication.sharedApplication()
+    # Initiate the controller with a XIB
+    viewController = mainWindow.alloc().initWithWindowNibName_("shoutout_main")
+    viewController.showWindow_(viewController)
+
+    # Bring app to top
+    NSApp.activateIgnoringOtherApps_(True)
 
     def applicationShouldHandleReopen_hasVisibleWindows_(self, theApplication, flag):
         print("werty")
 
-    def applicationDidFinishLaunching(self, aNotification):
+    def applicationDidFinishLaunching_(self, aNotification):
         print("Finished loading!")
