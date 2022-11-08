4044a5913b83f97629d58970d0892dbe57d40bf1
leifadev <49649192+leifadev@users.noreply.github.com>
2022-11-08 15:03:22 -0600

    added setup function, prints variables at help

--- src/AppDelegate.py
+++ src/AppDelegate.py
@@ -6,9 +6,12 @@ App Delegate, manages events about the app and the window controllers and so on.
 # Objective-C
 import Cocoa, objc
 from AppKit import *
+from Cocoa import NSLog
 
 # Shoutout modules
 from WindowController import mainWindow
+from sutils.tasks import backendTasks as tasks
+# from utils.langutils import Manager as langutils
 
 # Python modules
 # ---
@@ -16,23 +19,38 @@ from WindowController import mainWindow
 class AppDelegate(Cocoa.NSObject):
     """
     App delegate for Shoutout!
+    
     """
 
     app = Cocoa.NSApplication.sharedApplication()
 
-    # Initiate the controller with a XIB
     viewController = mainWindow.alloc().initWithWindowNibName_("shoutout_main")
     viewController.showWindow_(viewController)
+    # bundle = Foundation.NSBundle.mainBundle().loadNibNamed_owner_topLevelObjects_("shoutout_main", None, None)
+    # from pprint import pprint
+    # pprint(bundle[1][0:20])
 
     # Bring app to top
     NSApp.activateIgnoringOtherApps_(True)
 
     def applicationShouldHandleReopen_hasVisibleWindows_(self, theApplication, flag):
-        return True
+        print("Handling reopen...")
 
     def applicationDidFinishLaunching_(self, aNotification):
-        print("Finished loading!")
+        self.loadUIElements()
+
+    def loadUIElements(self):
+        """
+        Loads and sets all UI Elements on startup
+
+        :return:
+        """
+        yml_contents = tasks.getYAML()
 
 
+
+
+
+# Set this class as the delegate class!
 delegate = AppDelegate.alloc().init().retain()
 NSApplication.sharedApplication().setDelegate_(delegate)
