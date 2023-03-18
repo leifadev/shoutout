fb3fa29fe8d9de494f6dfdcb87f0808eddef312c
vanillahow <49649192+leifadev@users.noreply.github.com>
2023-03-18 16:12:05 -0500

    imported selectively more modules from Cocoa

--- src/notifications.py
+++ src/notifications.py
@@ -1,40 +1,43 @@
 """
-Responsible for summoning notifications from the Apple framework Gods
+Responsible for sending notifications from the Apple framework Gods
 
-Includes the new UserNotifications API that is compatible with MacOS 10.14/Mojave and up
-and includes all features available from attachments to options etc.
+Includes the new UserNotifications and NSUserNotifications frameworks for compatibility
+with Mojave and lower versions too.
 """
 
 from uuid import uuid4
-import UserNotifications as UN
-import Foundation, Cocoa
 import logging
-from Cocoa import NSLog
+
+import UserNotifications as UN
+from Cocoa import NSUserNotificationCenter, NSUserNotification
+from Cocoa import NSLog, NSURL, NSBundle
+import Foundation
+import objc
+
 
 def checkOldNotifications():
-    # Fetch version of MacOS
+    # Fetch version of macOS
     MacOSVersion = str(Foundation.NSProcessInfo.alloc().init().operatingSystemVersionString())
-    # MacOSVersion = 'Version 10.15.2 (Build 22A400)' # Testing string
+    # MacOSVersion = 'Version 10.13.2 (Build 22A400)'  # Testing string
     # Separate version number from string, concatenate into list of all the 3 numbers
     MacOSVersion = MacOSVersion.split(" ")[1].split(".")
     # Add major and minor number (move decimal to left twice) together
     MacOSVersion = int(MacOSVersion[0]) + int(MacOSVersion[1]) / 100
     print(MacOSVersion)
 
-    if MacOSVersion < 10.14: # Detect if the version number is higher than 10.14 (Mojave)
+    if MacOSVersion < 10.14:  # Detect if the version number is higher than 10.14 (Mojave)
         old_notifications = True
     else:
         old_notifications = False
     return old_notifications
 
 
-DEFAULT_AUTH_OPTIIONS = UN.UNAuthorizationOptions(UN.UNAuthorizationOptionBadge and UN.UNAuthorizationOptionSound)
-
+DEFAULT_AUTH_OPTIONS = UN.UNAuthorizationOptions(UN.UNAuthorizationOptionBadge) # and UN.UNAuthorizationOptionSound )
 
 class NotificationScheduler:
     center = UN.UNUserNotificationCenter.currentNotificationCenter()
 
-    def __init__(self):  # Init objects
+    def __init__(self):
         self.granted = None
         self.settings = None
         self.timeInterval = 1  # In minutes
@@ -47,15 +50,17 @@ class NotificationScheduler:
             while self.granted is None:
                 pass
 
-        return bool(self.granted)
+        return self.granted
 
     def _settingsHandler(self, settings):
         self.settings = settings
 
         # if settings.authorizationStatus() == UN.UNAuthorizationStatusDenied:
-        self.center.requestAuthorizationWithOptions_completionHandler_(DEFAULT_AUTH_OPTIIONS,
+        #     NSLog("Notifications are already denied, requesting for them again...")
+        # else:
+        #     NSLog("Requested Auth!")
+        self.center.requestAuthorizationWithOptions_completionHandler_(1,
                                                                        self._authorizationRequestHandler)
-        NSLog("Requested Auth!")
 
     def _authorizationRequestHandler(self, granted, error):
         self.granted = granted
@@ -64,13 +69,13 @@ class NotificationScheduler:
             NSLog(error)
 
     def _notificationRequestHandler(self, error):
-        # NSLog("_notificationRequestHandler")
+        NSLog("Handling error from a notification being requested")
         if error:
             NSLog(error)
 
     def sendNotificationRequest(self, title, subtitle, body):
         """
-        Sends a user notification with the User Notifications framework
+        Sends a user notification with the NSUserNotification framework
 
         “Send me a notiii!”
 
@@ -81,9 +86,12 @@ class NotificationScheduler:
         :param body: body text of the notification
         :return:
         """
+
+        # If this app is not authorized to send notifications
         if not self._haveAuthorization(block=True):
             NSLog("Error: Missing authorization to add notification request.")
-            return None
+
+            # PLEASE INSERT CODE TO SUCCESSFULLY REQUEST A SETTINGS THING YOU KNOW
 
         # Trigger repeats every minute (may not be using this though)
         # trigger = UN.UNTimeIntervalNotificationTrigger.triggerWithTimeInterval_repeats_(60, False)
@@ -99,8 +107,7 @@ class NotificationScheduler:
         # Attachments (logo)
         # import Foundation
         # fileURL = Foundation.NSURL.fileURLWithPath_("/Users/leif/PycharmProjects/shoutout/images/shoutout_logo.png")
-        # import Cocoa
-        # fileURL = Cocoa.NSURL.fileURLWithPath_(Cocoa.NSBundle.mainBundle().pathForResource_ofType_("shoutout_logo", "png"))
+        # fileURL = NSURL.fileURLWithPath_(NSBundle.mainBundle().pathForResource_ofType_("shoutout_logo", "png"))
         # attachments = UN.UNNotificationAttachment.attachmentWithIdentifier_URL_options_error_\
         #     ("Shoutout_image", fileURL, {}, None)
         # content.setAttachments_([attachments])
@@ -126,7 +133,7 @@ class NotificationScheduler:
 
     def sendNotificationRequestOld(self, title, subtitle, body):
         """
-        Sends a notfication with the NSUserNotifications framework for support for
+        Sends a notification with the NSUserNotifications framework for support for
         older versions. 10.14 (Mojave) and under.
 
         :param title: title of notification
@@ -134,32 +141,42 @@ class NotificationScheduler:
         :param body: body text of the notification
         :return:
         """
-        noti = Cocoa.NSUserNotification.alloc().init()
+        # Get instance of NSUserNotification from Cocoa
+        noti = NSUserNotification.alloc().init()
+
+        # Set texts, etc.
         noti.setTitle_(title)
         noti.setSubtitle_(subtitle)
         noti.setInformativeText_(body)
 
-        # Cocoa.NSUserNotificationCenter.defaultUserNotificationCenter().setDelegate_()
+        # NSUserNotificationCenter.defaultUserNotificationCenter().setDelegate_()
 
-        nc = Cocoa.NSUserNotificationCenter.defaultUserNotificationCenter()
+        # Get current notification center (just doing default one)
+        nc = NSUserNotificationCenter.defaultUserNotificationCenter()
+
+        # Send the notiiiii!
         nc.deliverNotification_(noti)
 
-        logging.info("Notification method has been called!")
+        NSLog("Notification method has been called!")
 
 
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
 if __name__ == "__main__":
-    notificationScheduler = NotificationScheduler()
-    if checkOldNotifications():
-        notificationScheduler.sendNotificationRequest("Shoutout!", f"Reminder", "You have a word of the day to check!")
-    else:
-        notificationScheduler.sendNotificationRequestOld("Shoutout!", f"Reminder", "You have a word of the day to check!")
+    from Cocoa import NSRunLoop
 
+    notificationScheduler = NotificationScheduler()
+
+    if checkOldNotifications():
+        notificationScheduler.sendNotificationRequestOld("Shoutout!", f"Reminder", "You have a word of the day to "
+                                                                                   "check!")
+    else:
+        notificationScheduler.sendNotificationRequest("Shoutout!", f"Reminder", "You have a word of the day to "
+                                                                                "check!")
     # Runs an event loop to keep the interpreter running to give Cocoa time to make a second thread for
     # the completion handler to run, thus so Python doesn't crash with an error. (zsh: illegal hardware instruction)
-    Cocoa.NSRunLoop.currentRunLoop()  # Returns the run loop for the current thread (once?)
+    NSRunLoop.currentRunLoop()  # Returns the run loop for the current thread (once?)
     # https://github.com/ronaldoussoren/pyobjc/issues/482
 
 # Time Interval Class
